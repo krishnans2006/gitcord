b392d943f385b50bac04aebaa18e95938e768fa2
Krishnan Shankar <krishnans2006@gmail.com>
2024-06-30 02:23:04 -0400

    Implement getting current defaults

--- gitcord/data/database.py
+++ gitcord/data/database.py
@@ -13,7 +13,7 @@ fb.initialize_app(cred)
 db: google.cloud.firestore_v1.client.Client = fb.firestore.client()
 
 
-def get_user_defaults(user_id: int, tag: str):
+def get_user_defaults(user_id: int, tag: str) -> dict:
     doc = db.collection("Users").document(str(user_id))
 
     doc.set({"Tag": tag}, merge=True)
@@ -25,5 +25,3 @@ def set_user_defaults(user_id: int, tag: str, defaults: dict):
     doc = db.collection("Users").document(str(user_id))
 
     doc.set({"Tag": tag, "Defaults": defaults}, merge=True)
-
-    return True
--- gitcord/slash/settings.py
+++ gitcord/slash/settings.py
@@ -2,6 +2,8 @@ import discord
 from discord import ApplicationContext, SlashCommandGroup
 from discord.ext import commands
 
+from gitcord.data.database import get_user_defaults
+
 
 class Settings(commands.Cog):
     def __init__(self, client: commands.Bot):
@@ -18,7 +20,16 @@ class Settings(commands.Cog):
         },
     )
     async def get_defaults(self, context: ApplicationContext) -> None:
-        await context.respond(content="Default settings are not yet implemented!", ephemeral=True)
+        defaults = get_user_defaults(context.author.id, str(context.author))
+        if not defaults:
+            await context.respond(
+                content=f"You haven't set your defaults yet! Use {self.set_defaults.mention} to do so"
+            )
+            return
+        content = ""
+        for key, value in defaults.items():
+            content += f"**{key}**: {value}\n"
+        await context.respond(content=content, ephemeral=True)
 
     @defaults.command(
         name="set",
